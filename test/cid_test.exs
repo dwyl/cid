246ca312b25dbceca3316a300d3878c8e2346e80
nelsonic <nelson+github@dwyl.io>
2018-12-03 21:51:33 +0000

    forgot to commit the tests ... derp!

--- test/cid_test.exs
+++ test/cid_test.exs
@@ -2,7 +2,13 @@ defmodule CidTest do
   use ExUnit.Case
   doctest Cid
 
-  test "greets the world" do
+  test "Creates a deterministic Content ID from Elixir String" do
     assert Cid.make("Elixir") == "NSqJspBr2u1F6z1DhcR2cnQAxLdQZBLk"
   end
+
+  test "Create a CID from a Map" do
+    map = %{cat: "Meow", dog: "Woof", fox: "What Does The Fox Say?"}
+    assert Cid.make(map) == "GdrVnsLSdxRphXgQgNsmq1FDyRXAySXT"
+  end
+
 end
