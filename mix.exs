70009a509d6d088124cbe7284801365722d98017
Robert Francis <robertefrancis18@gmail.com>
2019-01-30 19:31:38 +0000

    remove aliases #28

--- mix.exs
+++ mix.exs
@@ -8,15 +8,10 @@ defmodule Cid.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test,
         "coveralls.json": :test,
-        all_tests: :test
       ]
     ]
   end
@@ -36,13 +31,6 @@ defmodule Cid.MixProject do
       {:basefiftyeight, "~> 0.1.0"}, # Currenly building our own version of this here https://git.io/fhPaK. Can replace when it is ready
       {:excoveralls, "~> 0.10", only: :test},
       {:stream_data, "~> 0.4.2", only: :test}
-    ]
-  end
-
-  defp aliases do
-    [
-      test: ["coveralls --exclude ipfs"],
-      all_tests: ["coveralls.detail --include ipfs", "coveralls.json"]
     ]
   end
 end
