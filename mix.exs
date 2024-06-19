6d3cbaf29c2463388b0d75489da206b2f2ece080
Viktor Baranov <baranov.viktor.27@gmail.com>
2024-06-19 17:21:31 +0300

    Add :crypto dependency to extra_applications

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Cid.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :crypto]
     ]
   end
 
